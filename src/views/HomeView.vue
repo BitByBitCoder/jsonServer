c06d75800a2c40a8f6b63dce0c9f4f5225b98a49
HruaiaRalte <101803135+HruaiaRalte@users.noreply.github.com>
2024-01-14 20:45:41 +0530

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+let jobs = ref([]);
+onMounted(() => {
+  console.log("hell");
+  fetch("http://localhost:3000/jobs")
+    .then((res) => res.json())
+    .then((data) => (jobs.value = data));
+});
+</script>
+
+<template>
+  <h1></h1>
+  <div v-if="jobs">
+    <div v-for="jobs in jobs">
+      <span>name {{ jobs.title }}</span>
+    </div>
+  </div>
+</template>
+
+<style></style>
